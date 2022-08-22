21fe0e3129008bf3d94fe0e571ead83e2a4593d8
whonftw <raducristian118@gmail.com>
2022-08-22 21:51:11 +0300

    tests for query

---
+++ Basket.UnitTests/ContextFixture.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+
+namespace Basket.UnitTests
+{
+    public class ContextFixture
+    {
+        public static BasketDbContext SetupContext()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var dbContextOptions = new DbContextOptionsBuilder<BasketDbContext>()
+                .UseInMemoryDatabase($"Database{Guid.NewGuid()}")
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors()
+                .Options;
+
+            var dbContext = new BasketDbContext(dbContextOptions);
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+
+            return dbContext;
+        }
+
+        public static void SeedContext(BasketDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            context.Baskets.Add(new Domain.Entities.Basket { Customer = "Test", PaysVAT = true });
+            context.Items.Add(new Domain.Entities.Item { Name = "Test_item", BasketId = 1, Price = 10 });
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Basket.UnitTests/GetBasketItemsQueryHandlerUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using Basket.Application.Queries;
+using FluentAssertions;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Basket.UnitTests
+{
+    public class GetBasketItemsQueryHandlerUnitTests
+    {
+        private GetBasketItemsQueryHandler GetHandler()
+        {
+            var dbContext = ContextFixture.SetupContext();
+            ContextFixture.SeedContext(dbContext);
+            return new GetBasketItemsQueryHandler(dbContext);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public async Task GettingNonExistingBasket_ThrowsArgumentException(int basketId)
+        {
+            //Arrange
+            var handler = GetHandler();
+
+            //Act & Assert
+            await Assert.ThrowsAsync<ArgumentException>(async () => await handler.Handle(new GetBasketItemsQuery { BasketId = basketId }, new CancellationTokenSource().Token));
+        }
+
+        [Fact]
+        public async Task GettingExistingBasket_ShouldReturnBasketWithItem()
+        {
+            //Arrange
+            var handler = GetHandler();
+
+            //Act
+            var result = await handler.Handle(new GetBasketItemsQuery { BasketId = 1 }, new CancellationTokenSource().Token);
+
+            //Assert (hard-coded based on fixture seed)
+            result.Should().NotBeNull();
+            result.Items.Should().NotBeNullOrEmpty();
+            result.TotalNET.Should().Be(10);
+            result.TotalGross.Should().Be(11);
+        }
+    }
+}
