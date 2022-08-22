54548cdca8af375d2667767a70eaa51f9fb81e34
whonftw <raducristian118@gmail.com>
2022-08-22 22:43:01 +0300

    AddItemToBasket unit test

---
+++ Basket.UnitTests/AddItemToBasketCommandUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using Basket.Application.Commands;
+using Domain.Exceptions;
+using FluentAssertions;
+using Persistence;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Basket.UnitTests
+{
+    public class AddItemToBasketCommandUnitTests
+    {
+        private readonly BasketDbContext context = ContextFixture.SetupContext();
+
+        [Theory]
+        [InlineData(1, "test-item")]
+        public async Task AddItemToBasketCommand_ShouldAddItemToExistingBasket(int id, string item)
+        {
+            ContextFixture.SeedContext(context);
+
+            //Arrange
+            var handler = new AddItemToBasketCommandHandler(context);
+
+            //Act
+            await handler.Handle(new AddItemToBasketCommand { BasketId = id, Item = item, Price = 10 }, new CancellationTokenSource().Token);
+
+            //Assert
+            var basket = context.Items.FirstOrDefault(t => t.Name == item);
+            basket.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task AddItemToBasketCommand_ShouldThrowWhenPriceIsInvalid()
+        {
+            ContextFixture.SeedContext(context);
+
+            //Arrange
+            var handler = new AddItemToBasketCommandHandler(context);
+
+            //Act & Assert
+            await Assert.ThrowsAsync<InvalidPriceException>(async () => await handler.Handle(new AddItemToBasketCommand { BasketId = 1, Item = "test-invalid", Price = -1 }, new CancellationTokenSource().Token));
+        }
+    }
+}
