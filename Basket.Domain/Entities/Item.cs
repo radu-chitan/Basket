d466f3dd961b4d57fb3e1f062360f9a2d432353b
whonftw <raducristian118@gmail.com>
2022-08-22 19:41:00 +0300

    initial migration

--- Basket.Domain/Entities/Item.cs
+++ Basket.Domain/Entities/Item.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public int Price { get; set; }
+        public double Price { get; set; }
         public int BasketId { get; set; }
         public Basket? Basket { get; set; }
     }
