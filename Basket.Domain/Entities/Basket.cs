1005e6e9657314e7a839d6c2716a9d8c2d270079
whonftw <raducristian118@gmail.com>
2022-08-22 19:33:10 +0300

    add domain

---
+++ Basket.Domain/Entities/Basket.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public string Customer { get; set; } //IsRequired in BasketConfiguration
+        public bool PaysVAT { get; set; }
+        public ICollection<Item>? Items { get; set; }
+    }
+}
---
+++ Basket.Domain/Entities/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Price { get; set; }
+        public int BasketId { get; set; }
+        public Basket? Basket { get; set; }
+    }
+}
